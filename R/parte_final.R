c465e4e1d93e55f11620ef31014e616405188968
Luis-Romero1 <60513896+Luis-Romero1@users.noreply.github.com>
2021-02-08 20:06:41 -0600

    Update parte_final.R

--- R/parte_final.R
+++ R/parte_final.R
@@ -2,11 +2,18 @@
 library(tidyverse)
 library(stringi)
 
-#datos Cuenca
+#datos las Cuencas , los cuales vienen de CONAGUA donde cada variable significa lo siguiente
+#clv=Clave de la cuenca
+#nom_cue=Nombre de la cuenca
+#vmae=volumen medio anual de escurrimiento
+#vaeas= Volumen anual de extracción de aguas superficiales
+#RHA=Región hidrico adminsitrativa
+#http://sina.conagua.gob.mx/sina/tema.php?tema=cuencas&ver=mapa&o=0&n=nacional
 datos_agua <- read.csv("data/Cuencas con disponibilidad (2010-2020).csv", sep=";") %>% 
   as_tibble()
 
-#Seleccion de los datos Cuenca con disponibilad 
+#Seleccion de los datos Cuenca con disponibilad , omitimos los valores nulos y
+#cambiamos el nombre a CUENCA
 datos_agua_1<-
   datos_agua %>% 
   select(n,clv,nom_cue,vmae,vaeas,dma,a,year,RHA) %>% 
@@ -14,11 +21,19 @@ datos_agua_1<-
   rename(CUENCA = nom_cue)
 
 # ---------------------------------------------------------------------------------------------
+#Esta función lo que hara es poner la media para ciertos datos que tienen "-"
 
 na_impute <- function(x){
   ifelse(is.na(x), mean(x, na.rm = T), x)
 }
 
+#Aquí primero estandarizamos los datos de CUENCA poniendolos en minusculas,
+#sin acentos, sin espacios de más , sin puntos, luego por RHA CUENCA Y CLV 
+#se imputa la media en los "-" que despues de haber sido convertidos a NA 
+#se podran cambiar con facilidad con ayuda de la función, completamos la base de datos
+#para homogeneisar los datos ,para que al final no se presenten caidas en las graficas
+#por región hidrologica,  pueden estar disponibles lod datos del 2005 al 2020 
+# y al menos lo estan del 2005 al 2020
 datos_agua_11 <- datos_agua_1 %>% 
   select(-n) %>% 
   mutate(
@@ -34,16 +49,25 @@ datos_agua_11 <- datos_agua_1 %>%
   ungroup()
 
 # ---------------------------------------------------------------------------------------------
-
+#Esta base lo que contiene es un catalogo de clave municipal con 
+#municipios y paises, esto es para evitar duplicados más adelante
+#ya que hay cuencas con el mismo nombre pero diferente región hidrica
 cve_mun <- read.csv("data/clave_municipio.csv",sep=";") %>% 
   as_tibble()
 
+#Esta tabla lo que tiene es la precipitación anual que se obtuvo en el archivo 
+#Proyecto_agua1.R, que además viene por clave municipal y la RHA
 precipitacion <- read.csv("data/precipitacion_fin1.csv") %>% 
   as_tibble()
 
+#Esta tabla es un catalogo que conecta lo que es los municipios por cuenca, lo cual 
+#nos ayudara a asociar una "verdadera precipitación" por cuenca(aunque despues se 
+#va a tener que sacar la media por cuenca y RHA)
 municipios <- read.csv("data/Catalogo_CLICOM_Precipitacion (2).csv") %>% 
   as_tibble()
 
+#En esta parte empezamso normalizando los nombres de las cuencas por acentos, puntos , poner todo en minusculas
+#sin espaccios de más,quitando vacios.
 municipios <- 
   municipios %>% 
   mutate(
@@ -62,6 +86,8 @@ municipios <-
     sep = ""
   )
 
+#Ahora a estos municipios les vamos a pegar la región hidrológica administrativa, además de pegarle 
+# las precipitaciones anuales por año
 municipiosRH <- cve_mun %>% 
   mutate(
     mun_est = stri_trans_general(tolower(gsub(" ","",mun_est)),"Latin-ASCII")
@@ -71,10 +97,14 @@ municipiosRH <- cve_mun %>%
   inner_join(precipitacion)
 
 # ---------------------------------------------------------------------------------------------
-
+#Aquí ya se le pega como tal las precipitaciones anuales por municipio por año asociada a cada cuenca 
+#que hay en la base de datos , siendo lo más importante hasta el momento las variables
+#anual, vmea,vaeas,a, pero este anual esta por municipio no por cuenca.
 datos_agua_2 <- municipiosRH %>% 
   right_join(datos_agua_11, by = c("RHA", "CUENCA", "Año" = "year"))
 
+
+#Esta base de datos servira para pregarle la región hidrologica=RH
 cuencas_rh <- XML::xmlToDataFrame("http://201.116.60.29/servicios/api/disponibilidadcuencashidrologicas/2017") %>% 
   as_tibble() %>% 
   select(Id_RHA, RHA, Id_rh, nom_rh, CUENCA = Nombre) %>% 
@@ -82,6 +112,10 @@ cuencas_rh <- XML::xmlToDataFrame("http://201.116.60.29/servicios/api/disponibil
     across(CUENCA, ~stri_trans_general(.,id = "Latin-ASCII") %>% tolower() %>% str_remove_all("\\.") %>% str_squish())
   )
 
+#Con esta base de datos lo que queremos es pegarle la región hidrológica administrativa a cada cuenca, ya que es 
+#necesaria para lo que finalmente se va a reportar, además de calculas las precipitaciones asociadas a cada cuenca 
+#de la siguiente manera , si una cuenca esta relacionada a tres municipios , su precipitación anual sera la mediana
+#de los datos por año
 datos_agua_21 <- datos_agua_2 %>% 
   left_join(cuencas_rh) %>% 
   arrange(RHA, CUENCA, Año) %>% 
@@ -95,12 +129,17 @@ datos_agua_21 <- datos_agua_2 %>%
 precipitacion_total <- datos_agua_21 %>% 
   summarise(Anual = median(Anual, na.rm = T))
 
+#Aquí es donde ya tenemos ese concentrado de variables Anual,vmea,vaeas,a, donde ahora si la precipitación es por cuenca
 datos_aguaF <- datos_agua_21 %>% 
   slice(1) %>% 
   select(-(Cve_Mpio:Anual)) %>% 
   left_join(precipitacion_total)
 
-# ---------------------------------------------------------------------------------------------
+# Datos necesarios para las predicciones-------------
+
+#Aquí fue donde tomamos muchas decisiones , primero para el área=a que tendria asociada una cuenca de los años 2021 a 2023 se utilizo medias moviles 
+#luego se calculo un coeficiente k=a*Anual donde Anual es la precipitación anual por cuenca , ya que este coeficiente nos ayudara a calcular 
+#la el coeficiente vmae=a*Anual*ce donde ce es un coeficiente afectado por el terreno donde esta la cuenca 
 
 RHF <- datos_aguaF %>%
   ungroup() %>%
